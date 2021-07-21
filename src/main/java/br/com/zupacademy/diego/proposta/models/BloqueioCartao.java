72b3c2637915f3d356cbf0f070043b402dd93586
Diego Batista <diego.silva@aluno.ic.ufmt.br>
2021-07-21 16:11:54 -0400

    feat: configuração keycloak

---
+++ src/main/java/br/com/zupacademy/diego/proposta/models/BloqueioCartao.java
@@ -0,0 +1,17 @@
+package br.com.zupacademy.diego.proposta.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_bloqueio_cartao")
+public class BloqueioCartao {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+}
